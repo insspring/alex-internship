95520fa31bc116853c10b29e12190cd94c369a83
Alex <beldeut@gmail.com>
2020-02-03 12:53:07 +0300

    add everything

---
+++ front-end-internship-february-2020/front-end/src/main.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import i18n from "./i18n";
+import axios from "axios";
+
+Vue.config.productionTip = false;
+axios.defaults.baseURL = 'http://localhost:3000';
+
+new Vue({
+    router,
+    store,
+    i18n,
+    render: h => h(App)
+}).$mount("#app");
